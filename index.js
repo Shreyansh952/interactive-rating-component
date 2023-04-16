9dd6c9020088cfa7f96d0832b9367afaf1fee079
Shreyansh Swarnkar <you@example.com>
2023-04-16 17:12:44 +0530

    modifications with html, css and js file

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-$(".result-card").hide();
+$(".result-card, .note").hide();
 var score;
-$("button.bg-circle").click(function(){
-    score = $(this).text();
-})
+$(document).click(function(event){
+    if($(event.target).parent().hasClass("rating-list")){
+        score = $(event.target).text();
+    }else{
+        score = undefined;
+    }
+});
 
 $(".submit-btn").click(function(){
+    if(score==undefined){
+        $(".note").show();
+    }else{
+     $(".note").hide();
     $(".points").text(score+" "); 
     $(".first-card").hide();
     $(".result-card").show();
-})
+    }
+});
